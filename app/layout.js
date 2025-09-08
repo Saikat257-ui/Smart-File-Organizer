17e86e5c0b9c8cc8cd1a2bcda0d5dae860c96242
Saikat257-ui <saikatp571@gmail.com>
2025-09-08 20:33:54 +0530

    Add microsoft clarity analytics script

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "t7hjfcwv8c");
+            })(window, document, "clarity", "script", "t7lp0n9ybn");
           `
         }} />
       </head>
